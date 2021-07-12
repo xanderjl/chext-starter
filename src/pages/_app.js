5494cfad3559760e4aa4aeea840906a87a5797c7
Xander Low <alexanderjameslow@gmail.com>
2021-07-12 11:25:29 -0400

    added: default next-seo config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,27 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import { DefaultSeo } from "next-seo"
+
 import theme from "@theme/index"
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo
+        title="A Spicy Title 🔥"
+        titleTemplate="%s | Your New Site"
+        description="A lovely description of this new site you've made."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://www.url.ie/",
+          site_name: "SiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
